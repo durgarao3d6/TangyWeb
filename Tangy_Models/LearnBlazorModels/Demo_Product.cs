a6f471114d3fdced413cfe23acd2b9a0964c20bf
durgarao3d6 <durgarao3d6@gmail.com>
2022-08-07 19:13:07 +0530

    Add project files.

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangy_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProductId { get; set; }  
+        public decimal Price { get; set; }
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
+    }
+}
